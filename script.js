e775dcccee35447f8b4b8212d35f233ea40686ee
João <joaovitorleonardo8@gmail.com>
2023-03-13 15:14:57 -0300

    update

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ function scrollToSection() {
     var section = document.getElementById("secondary-content");
     section.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
 }
+
+$(document).ready(function() {
+    $('a').on('click', function() {
+      $('.container-fluid').collapse('hide');
+    });
+});
+  
